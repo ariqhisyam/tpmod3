d00f34f234c33a3e288111b7756c45e270e7fe34
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-03-02 15:42:14 +0700

    Tambahan Fitur GUI

--- tpmodd3/Form1.Designer.cs
+++ tpmodd3/Form1.Designer.cs
@@ -26,7 +26,7 @@
             this.textBox1.Size = new System.Drawing.Size(200, 23);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            
+
             this.button1.Location = new System.Drawing.Point(50, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
