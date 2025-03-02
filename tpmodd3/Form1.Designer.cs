6cb72c68c84e5ef0bc408765bd0f92e1be83d5b5
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-03-02 15:38:45 +0700

    Version 1

--- tpmodd3/Form1.Designer.cs
+++ tpmodd3/Form1.Designer.cs
@@ -20,17 +20,13 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
+           
             this.textBox1.Location = new System.Drawing.Point(50, 50);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(200, 23);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
+            
             this.button1.Location = new System.Drawing.Point(50, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -38,9 +34,6 @@
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(300, 200);
